8425d2be1acd566f08447469621fb794524c2c57
zheng7426 <lilsam6670@gmail.com>
2020-04-02 15:49:34 -0400

    data updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,6 +16,7 @@ users = User.create! [
                      ]
 
 Book.create! [
+                 # hardcover non-fiction
                  { title: 'The Mamba Mentality',
                    author: 'Kobe Bryant',
                    summary: 'Various skills and techniques used on the court by the late Los Angeles Lakers player.',
@@ -29,7 +30,101 @@ Book.create! [
                    genre: 'hardcover-nonfiction',
                    isbn: '9783641227326',
                    user_id: users.sample.id
-                 }
+                 },
+                 { title: 'Lady in Waiting',
+                   author: 'Anne Glenconner',
+                   summary: 'A memoir that provides a look into the royal family by a lady-in-waiting to Princess Margaret.',
+                   genre: 'hardcover-nonfiction',
+                   isbn: '9780306846359',
+                   user_id: users.sample.id
+                 },
+                 { title: 'The Office',
+                   author: 'Andy Greene',
+                   summary: 'An oral history of the sitcom from its beginnings on the BBC through its nine-season run on American network TV.',
+                   genre: 'hardcover-nonfiction',
+                   isbn: '9781524744991',
+                   user_id: users.sample.id
+                 },
+                 { title: 'Unknown Valor',
+                   author: 'Martha MacCallum',
+                   summary: 'The Fox News anchor weaves stories of combat veterans who fought during World War II.',
+                   genre: 'hardcover-nonfiction',
+                   isbn: '9780062853875',
+                   user_id: users.sample.id
+                 },
+                 # Animals
+                 { title: 'H Is For Hawk',
+                   author: 'Helen Macdonald',
+                   summary: 'A grief-stricken British woman decides to raise a goshawk, a fierce bird that is notoriously difficult to tame.',
+                   genre: 'animals',
+                   isbn: '9781448130726',
+                   user_id: users.sample.id
+                 },
+                 { title: 'The Genius of Birds',
+                   author: 'Jennifer Ackerman',
+                   summary: 'Research reveals that many birds rival primates and even humans in their complex forms of intelligence.',
+                   genre: 'animals',
+                   isbn: '9780399563126',
+                   user_id: users.sample.id
+                 },
+                 { title: 'The Soul of An Octopus',
+                   author: 'Sy Montgomery',
+                   summary: 'The complex intelligence, emotional capacities, playfulness and spirit of the octopus, by a naturalist who counts several as friends.',
+                   genre: 'animals',
+                   isbn: '9781622316168',
+                   user_id: users.sample.id
+                 },
+                 { title: 'A Street Cat Named Bob',
+                   author: 'James Bowen',
+                   summary: 'A London busker befriends an injured cat, and the two become inseparable.',
+                   genre: 'animals',
+                   isbn: '9781250029478',
+                   user_id: users.sample.id
+                 },
+                 { title: 'The Lion In The Living Room',
+                   author: 'Abigail Tucker',
+                   summary: 'The evolutionary science, natural history and biology of house cats.',
+                   genre: 'animals',
+                   isbn: '9781501154478',
+                   user_id: users.sample.id
+                 },
+                 #Hardcover Fiction
+                 { title: 'The Glass Hotel',
+                   author: 'Emily St John Mandel',
+                   summary: 'Years after an international Ponzi scheme falls apart, one of its victims investigates the disappearance of a woman from a container ship.',
+                   genre: 'hardcover-fiction',
+                   isbn: '9780525521150',
+                   user_id: users.sample.id
+                 },
+                 { title: 'In Five Years',
+                   author: 'Rebecca Serle',
+                   summary: 'A Manhattan lawyer finds herself confronting a vision she had when elements of it come to life on schedule.',
+                   genre: 'hardcover-fiction',
+                   isbn: '9781982137465',
+                   user_id: users.sample.id
+                 },
+                 { title: 'My Dark Vanessa',
+                   author: 'Kate Elizabeth Russell',
+                   summary: 'A woman re-evaluates the relationship she had at age 15 with her 42-year-old English teacher 17 years ago.',
+                   genre: 'hardcover-fiction',
+                   isbn: '9780062941527',
+                   user_id: users.sample.id
+                 },
+                 { title: 'The Silent Patient',
+                   author: 'Alex Michaelides',
+                   summary: 'Theo Faber looks into the mystery of a famous painter who stops speaking after shooting her husband.',
+                   genre: 'hardcover-fiction',
+                   isbn: '9781250301710',
+                   user_id: users.sample.id
+                 },
+                 { title: 'The Boy From The Woods',
+                   author: 'Harlan Coben',
+                   summary: 'When a girl goes missing, a private investigator’s feral childhood becomes an asset in the search.',
+                   genre: 'hardcover-fiction',
+                   isbn: '',
+                   user_id: users.sample.id
+                 },
+
              ]
 
 becoming = Book.find_by(title: 'Becoming')
