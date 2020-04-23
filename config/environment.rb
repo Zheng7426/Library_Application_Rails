537fc28e02bfa52ea00770d046db433462a6682c
zheng7426 <lilsam6670@gmail.com>
2020-04-23 15:03:11 -0400

    smtp sandgrid setup

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,16 @@
 # Load the Rails application.
 require_relative 'application'
 
+#Sendgrid Configuration
+ActionMailer::Base.smtp_settings = {
+    :user_name => 'apiKey',
+    :password => Rails.application.secrets.sendgrid_api_key,
+    :domain => 'yourdomain.com',
+    :address => 'smtp.sendgrid.net',
+    :port => 465,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+}
+
 # Initialize the Rails application.
 Rails.application.initialize!
